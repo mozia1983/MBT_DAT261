a7a47343c8bde9a47bc67d6f07f9a4b878ae9b8b
mohamedhas <d.mohmspb@gmail.com>
2018-05-05 15:12:45 +0200

    example code

--- src/test/java/PropTest.java
+++ src/test/java/PropTest.java
@@ -1,2 +1,16 @@
-public class PropTest {
+import org.quicktheories.WithQuickTheories;
+import org.testng.annotations.Test;
+
+import static org.quicktheories.generators.Generate.*;
+
+public class PropTest implements WithQuickTheories {
+
+    @Test
+    public void test() {
+        qt()
+                .forAll(range(1, 102), constant(7))
+                .check((i, c) -> i + c >= 7);
+    }
+
+    
 }
